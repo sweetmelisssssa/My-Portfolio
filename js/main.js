75cc3b63c32364327f30b07afd7211341c5e37ce
Melissa Hoffman <melissa@heylittlesisterdesign.com>
2016-11-05 12:56:04 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,27 +1,47 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+console.log( "ready!" );
 
-var requestURL = "https://sweetmelisssssa.github.io/my-portfolio/data.json";
+	var requestURL = "https://sweetmelisssssa.github.io/my-portfolio/data.json";
 
-$.ajax({
-  method: "GET",
-  url: requestURL,
-  success: mySuccessListener,
-  error: myErrorListener,
-  dataType: 'json'
+	$.ajax({
+	  method: "GET",
+	  url: requestURL,
+	  success: mySuccessListener,
+	  error: myErrorListener,
+	  dataType: 'json'
 
+	});
+
+		function mySuccessListener(data)
+		{
+		drawAllProject(data.projects);
+
+		}
+
+		function myErrorListener(xhr, ajaxOptions, thrownError)
+		{
+		console.log(thrownError);
+		}
 });
+	
+		function drawAllProject(projects)
+		{
 
-function mySuccessListener(data)
-{
-alert('success');
-}
+		for( var i = 0; i < projects.length; i++) {
+			
+			var projectString = drawProject(projects[i]);
+			$("#hlsd-project-container").append(projectString);
+			alert(projectString);
+		}
+	
+		}
 
-function myErrorListener(xhr, ajaxOptions, thrownError)
-{
-alert(thrownError);
+function drawProject(project)
+		{
+		
+		var aux = '<div class="col-sm-4"><img class="hlsd-index-img" src="'+project.thumb+'" width ="275px"><h3 id="hlsd-index-h3"><a href="#">Powerful Harvest</a></h3></div>';
 
-}
+		return aux;
 
-});
-
+		}
+		
