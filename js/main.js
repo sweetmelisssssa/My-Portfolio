956cf89318ca43b9f2dd6e8e4e9fed34892f6db1
Melissa Hoffman <melissa@heylittlesisterdesign.com>
2016-11-05 12:00:28 -0400

    update json again

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+var requestURL = "https://sweetmelisssssa.github.io/my-portfolio/data.json";
+
+$.ajax({
+  method: "GET",
+  url: requestURL,
+  success: mySuccessListener,
+  error: myErrorListener,
+  dataType: 'json'
+
+});
+
+function mySuccessListener(data)
+{
+alert('success');
+}
+
+function myErrorListener(xhr, ajaxOptions, thrownError)
+{
+alert(thrownError);
+
+}
+
+});
 
