4b2e4517186d5795e2386b4da8bcd7f7de73ea40
Melissa Hoffman <melissa@heylittlesisterdesign.com>
2016-11-15 21:21:02 -0500

    update

--- js/hlsd-index.js
+++ js/hlsd-index.js
@@ -14,7 +14,7 @@ console.log( "ready!" );
 
 		function mySuccessListener(data)
 		{
-		drawAllProject(data.projects);
+		drawAllProject(data.projectTypeList);
 
 		}
 
@@ -24,24 +24,51 @@ console.log( "ready!" );
 		}
 
 
-		$.urlParam = function(name){
-var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+		$.urlParam = function(typeName){
+var results = new RegExp('[\?&]' + typeName + '=([^&#]*)').exec(window.location.href);
 return results[1] || 0;
 }
 
 
 });
 	
-		function ()
+		function drawAllProject(projectTypeList)
 		{
 
-			
+			var closeString = "</div><div class='row'>";
+			var aux = 0;
+
+			$("#hlsd-project-container").append("<div class='row'>");
+			console.log("opening project")
+			for( var i = 0; i < projectTypeList.length; i++) {
+				aux++;
+				
+				var projectString = drawProject(projectTypeList[i], i);
+
+				$("#hlsd-project-container").append(projectString);
+				console.log("drawing project");
+
+
+				if(aux==3)
+
+				{
+					aux = 0;
+					$("#hlsd-project-container").append(closeString);
+					console.log("closing/opening row")
+				}
+
+			}
+
+			$("#hlsd-project-container").append("</div>");
+			console.log("closing last row")
 		}
 
-		function ()
+		function drawProject(projectTypeList, projectNumber)
 		{
 		
+		var aux = '<div class="col-sm-4"><img class="hlsd-index-img" src="'+projectTypeList.typeImage+'" width ="275px"><h3 id="hlsd-index-h3"><a href="logo-list-page.html?id='+projectNumber+'"">Logos</a></h3></div>'
+
+
+		return aux;
 
 		}
-
-		
