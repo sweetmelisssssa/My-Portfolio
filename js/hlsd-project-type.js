14625058d60fe4ba582435ad9f387505fcc388f9
Melissa Hoffman <melissa@heylittlesisterdesign.com>
2016-11-17 21:26:20 -0500

    update14

--- js/hlsd-project-type.js
+++ js/hlsd-project-type.js
@@ -14,7 +14,7 @@ console.log( "ready!" );
 
 		function mySuccessListener(data)
 		{
-		drawAllProject(data.projects);
+		drawAllProject(data.projectTypeList);
 
 		}
 
@@ -24,7 +24,7 @@ console.log( "ready!" );
 		}
 
 
-		$.urlParam = function(name){
+		$.urlParam = function(typeName){
 var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
 return results[1] || 0;
 }
@@ -32,16 +32,44 @@ return results[1] || 0;
 
 });
 	
-		function ()
+		function drawAllProject(projectType)
 		{
 
-			
+			var closeString = "</div><div class='row'>";
+			var aux = 0;
+
+			$("#hlsd-project-container").append("<div class='row'>");
+			console.log("opening project")
+			for( var i = 0; i < projectType.length; i++) {
+				aux++;
+				
+				var projectString = drawProject(projectType[i], i);
+
+				$("#hlsd-project-container").append(projectString);
+				console.log("drawing project");
+
+
+				if(aux==3)
+
+				{
+					aux = 0;
+					$("#hlsd-project-container").append(closeString);
+					console.log("closing/opening row")
+				}
+
+			}
+
+			$("#hlsd-project-container").append("</div>");
+			console.log("closing last row")
 		}
 
-		function ()
+		function drawProject(projectType, projectNumber)
 		{
 		
+		var aux = '<div class="col-sm-4"><img class="hlsd-index-img" src="'+projectType.typeImage+'" width ="275px"><h3 id="hlsd-index-h3"><a href="logo-list-page.html?id='+projectType+'">'+projectTypeList.typeName+'</a></h3></div>';
+
+'<div class="col-sm-4"><img class="hlsd-logo-list-page-img" src="+projectType.thumb+" width ="275px"><h3 class="logo-list-page-logo-name?id='+projectType+'">'+projectTypeList.name+'</h3><p class="logo-list-page-description">'+projectTypeList.description+'</p></div>';
+
+		return aux;
 
 		}
-
-		
