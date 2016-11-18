ce689bf37eaa1f0b16ca45b28cf3a1613467cdd8
Melissa Hoffman <melissa@heylittlesisterdesign.com>
2016-11-18 11:21:35 -0500

    update2

--- js/hlsd-project-type.js
+++ js/hlsd-project-type.js
@@ -63,7 +63,7 @@ return results[1] || 0;
 			console.log("closing last row")
 		}
 
-		function drawProject(projectType, projectNumber)
+		function drawProject(projectType)
 		{
 		
 		var aux = '<div class="col-sm-4"><img class="hlsd-logo-list-page-img" src="'+projectType.thumb+'" width ="275px"><h3 id="logo-list-page-logo-name">'+projectType.name+'</h3><p id="logo-list-page-description">'+projectType.description+'</p></div>';
